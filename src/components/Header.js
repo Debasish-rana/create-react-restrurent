83df00979a2518d88c5105991072c82ca1687b40
Debashish Rana <152690128+Debasish-rana@users.noreply.github.com>
2024-08-28 10:09:40 +0530

    #change in cart

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,15 @@
 import { LOGO_URL } from "./util/url";
 import { Link } from "react-router-dom";
 import UserContext from "./util/UserContext";
-import { useState,useContext } from "react";
-import {useSelector} from "react-redux"
-
+import { useState, useContext } from "react";
+import { useSelector } from "react-redux";
 
 const Header = () => {
-  const [logBtn, setLogBtn] = useState("Login")
-  const data = useContext(UserContext)
-  const cartItems = useSelector((store)=>store.cart.items)
+  const [logBtn, setLogBtn] = useState("Login");
+  const data = useContext(UserContext);
+  const cartItems = useSelector((store) => store.cart.items);
   console.log(cartItems);
-  
+
   //console.log(data.loggedInUser);
   return (
     <div className="Header-container flex justify-between bg-blue-200 shadow-xl mb-4 list-none sticky top-0">
@@ -19,12 +18,37 @@ const Header = () => {
       </div>
       <div className="list-item">
         <ul className="flex m-6 gap-8 ">
-          <li><Link to="/create-react-restrurent/home">Home</Link></li>
-          <li><Link to="/create-react-restrurent/about">About Us</Link></li>
-          <li><Link to="/create-react-restrurent/cotact">Contact Us</Link></li>
-          <li><Link to={"/create-react-restrurent/cart"}>cart - <span className="font-bold">({cartItems.length} items)</span></Link></li>
           <li>
-            <button onClick={()=>{logBtn === "Login"?setLogBtn("Logout"):setLogBtn("Login")}}>{logBtn}</button>
+            <Link to="/create-react-restrurent/home">Home</Link>
+          </li>
+          <li>
+            <Link to="/create-react-restrurent/about">About Us</Link>
+          </li>
+          <li>
+            <Link to="/create-react-restrurent/cotact">Contact Us</Link>
+          </li>
+          <li>
+            <Link to={"/create-react-restrurent/cart"}>
+            <div className="flex">
+              <div>
+                <img
+                  className="w-10"
+                  src="https://www.freeiconspng.com/uploads/blue-shopping-cart-icon-29.png"
+                  alt="blue shopping cart icon"
+                />
+              </div>
+              <div className="font-bold">{cartItems.length}</div>
+              </div>
+            </Link>
+          </li>
+          <li>
+            <button
+              onClick={() => {
+                logBtn === "Login" ? setLogBtn("Logout") : setLogBtn("Login");
+              }}
+            >
+              {logBtn}
+            </button>
           </li>
           <li>{data.loggedInUser}</li>
         </ul>
